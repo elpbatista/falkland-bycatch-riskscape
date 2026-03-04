f20dbd74578f091bdef6358ac34d2f295279db74
elpbatista <elpbatista@gmail.com>
2026-03-04 01:45:15 -0800

    download base

---
+++ src/riskscape/download/base.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import datetime
+import requests
+
+from riskscape.config import cfg, paths
+
+
+def daterange(start, end):
+    current = start
+    while current <= end:
+        yield current
+        current += datetime.timedelta(days=1)
+
+
+def download_file(url, outfile):
+
+    if outfile.exists():
+        return
+
+    r = requests.get(url)
+
+    if r.status_code == 200:
+        with open(outfile, "wb") as f:
+            f.write(r.content)
+    else:
+        print("Failed:", url)
+
+
+def run_downloader(dataset_name, build_url, build_filename):
+
+    start = datetime.date.fromisoformat(cfg["time"]["start"])
+    end = datetime.date.fromisoformat(cfg["time"]["end"])
+
+    raw_dir = paths["raw"] / dataset_name
+    raw_dir.mkdir(parents=True, exist_ok=True)
+
+    for date in daterange(start, end):
+
+        filename = build_filename(date)
+        url = build_url(date)
+
+        outfile = raw_dir / filename
+
+        print("Downloading:", filename)
+
+        download_file(url, outfile)
