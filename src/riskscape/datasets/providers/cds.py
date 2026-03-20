68de184e6589420ea6e908a36ce59f06c590ccb6
elpbatista <elpbatista@gmail.com>
2026-03-19 19:17:58 -0700

    adding wind data

--- src/riskscape/datasets/providers/cds.py
+++ src/riskscape/datasets/providers/cds.py
@@ -28,7 +28,7 @@ def buffered_bbox():
 
 
 def download(dataset_cfg, dataset_dir):
-    """Download dataset from CDS."""
+    """Download dataset from CDS in monthly chunks."""
 
     product = dataset_cfg["product"]
     variables = dataset_cfg["variables"]
@@ -47,32 +47,33 @@ def download(dataset_cfg, dataset_dir):
     client = cdsapi.Client()
 
     for year in range(start_year, end_year + 1):
+        for month in range(1, 13):
 
-        output_file = dataset_dir / f"{product}_{year}.nc"
+            output_file = dataset_dir / f"{product}_{year}_{month:02d}.nc"
 
-        if output_file.exists():
-            print("Already exists:", year)
-            continue
+            if output_file.exists():
+                print("Already exists:", year, f"{month:02d}")
+                continue
 
-        print("Downloading:", product, year)
+            print("Downloading:", product, year, f"{month:02d}")
 
-        client.retrieve(
-            product,
-            {
-                "product_type": "reanalysis",
-                "variable": variables,
-                "year": str(year),
-                "month": [f"{m:02d}" for m in range(1, 13)],
-                "day": [f"{d:02d}" for d in range(1, 32)],
-                "daily_statistic": "daily_mean",
-                "time_zone": "UTC+00:00",
-                "area": [
-                    ymax,  # north
-                    xmin,  # west
-                    ymin,  # south
-                    xmax,  # east
-                ],
-                "format": "netcdf",
-            },
-            str(output_file),
-        )
+            client.retrieve(
+                product,
+                {
+                    "product_type": "reanalysis",
+                    "variable": variables,
+                    "year": str(year),
+                    "month": f"{month:02d}",
+                    "day": [f"{d:02d}" for d in range(1, 32)],
+                    "daily_statistic": "daily_mean",
+                    "time_zone": "UTC+00:00",
+                    "area": [
+                        ymax,  # north
+                        xmin,  # west
+                        ymin,  # south
+                        xmax,  # east
+                    ],
+                    "format": "netcdf",
+                },
+                str(output_file),
+            )
