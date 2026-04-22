343ae8a80c61490ee2a25575f8dc074144aa2b6f
elpbatista <elpbatista@gmail.com>
2026-04-22 00:47:36 -0700

    refactoring

--- scripts/dev/test_cds_api.py
+++
@@ -1,2 +0,0 @@
-import cdsapi
-cdsapi.Client()
--- scripts/test_zip.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-
-z = zipfile.ZipFile(
-    "data/raw/wind/derived-era5-single-levels-daily-statistics_2014_01.zip"
-)
-
-print(z.namelist())
