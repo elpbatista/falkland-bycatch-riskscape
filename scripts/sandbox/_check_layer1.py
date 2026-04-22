815a59826f1f337201b0f3509c9a89401de4d267
elpbatista <elpbatista@gmail.com>
2026-04-22 00:39:27 -0700

    refactoring

--- scripts/sandbox/_check_layer1.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df = pd.read_parquet("data/layer1/year=2014.parquet")
-
-print(len(df))
-print(df.groupby("date").size().head())
--- scripts/sandbox/_test_grid.py
+++
@@ -1,7 +0,0 @@
-import geopandas as gpd
-
-gdf = gpd.read_parquet(
-    "data/grids/h3_res6_falkland_islands.parquet"
-)
-
-print(gdf.head())
